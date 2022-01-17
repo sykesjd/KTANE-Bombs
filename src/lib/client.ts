c4733f1eb99e3963304e2c9552187719bc143051
samfundev <samdinner@hotmail.com>
2022-01-16 23:19:27 -0500

    Fix npm run build not working

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+// Fix for `npm run build` not working.
+// See: https://github.com/prisma/prisma/pull/4920#issuecomment-927334909
+import pkg, { PrismaClient } from '@prisma/client';
 
-const client = new PrismaClient();
+let client;
+
+if (process.env.NODE_ENV === 'production') {
+	const { PrismaClient: PrismaClientProd } = pkg;
+	client = new PrismaClientProd();
+} else {
+	client = new PrismaClient();
+}
 
 export default client;
