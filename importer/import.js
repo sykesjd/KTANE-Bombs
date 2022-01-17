087e12e30d920c96d27dfc585ca77ee4342395fd
samfundev <samdinner@hotmail.com>
2022-01-16 23:37:51 -0500

    Updated importer

--- importer/import.js
+++ importer/import.js
@@ -5,18 +5,20 @@ const { PrismaClient } = pkg;
 (async function () {
 	const client = new PrismaClient();
 
-	const missions = JSON.parse(readFileSync('bombs.json').toString());
-	for (const mission of missions) {
-		await client.mission.create({
-			data: {
-				name: mission.name,
-				bombs: {
-					create: mission.bombs
-				},
-				completions: {
-					create: mission.completions
+	const packs = JSON.parse(readFileSync('bombs.json').toString());
+	for (const pack of packs) {
+		for (const mission of pack.missions) {
+			await client.mission.create({
+				data: {
+					name: mission.name,
+					bombs: {
+						create: mission.bombs
+					},
+					completions: {
+						create: mission.completions
+					}
 				}
-			}
-		});
+			});
+		}
 	}
 })();
