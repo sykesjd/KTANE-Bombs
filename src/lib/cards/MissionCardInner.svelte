160535718b5d24ee2f95337dbd73825a4f568d4c
LuminoscityTim <luminoscity@gmail.com>
2022-11-18 04:35:46 -0800

    pluralize mission attributes in MissionCard

--- src/lib/cards/MissionCardInner.svelte
+++ src/lib/cards/MissionCardInner.svelte
@@ -1,38 +1,36 @@
 <script lang="ts">
 	import { Bomb } from '$lib/types';
 	import type { Mission } from '$lib/types';
-	import { formatTime, getSolveTypes, listify } from '$lib/util';
+	import { formatTime, getSolveTypes, listify, pluralize } from '$lib/util';
 
 	export let mission: Mission;
 	const solveTypes = getSolveTypes(mission);
 
 	const bombs = mission.bombs;
 	const statBomb = new Bomb();
-	statBomb.modules = bombs.map((bomb) => bomb.modules).reduce((a, b) => a + b, 0);
-	statBomb.time = Math.max(...bombs.map((bomb) => bomb.time));
-	statBomb.strikes = Math.max(...bombs.map((bomb) => bomb.strikes));
-	statBomb.widgets = Math.max(...bombs.map((bomb) => bomb.widgets));
+	statBomb.modules = bombs.map(bomb => bomb.modules).reduce((a, b) => a + b, 0);
+	statBomb.time = Math.max(...bombs.map(bomb => bomb.time));
+	statBomb.strikes = Math.max(...bombs.map(bomb => bomb.strikes));
+	statBomb.widgets = Math.max(...bombs.map(bomb => bomb.widgets));
 
 	const orBlank = (condition: boolean, color: string) => (condition ? color : 'transparent');
 
-	const color = `linear-gradient(${orBlank(
-		solveTypes.normalSolve,
-		'hsl(210, 100%, 65%)'
-	)} 25%, ${orBlank(solveTypes.efmSolve, 'hsl(300, 100%, 75%)')} 25.01% 50%, ${orBlank(
-		mission.tpSolve,
-		'#9146ff'
-	)} 50.01% 75%, ${orBlank(solveTypes.soloSolve, '#00ffff')} 75.01%)`;
+	const color = `linear-gradient(${orBlank(solveTypes.normalSolve, 'hsl(210, 100%, 65%)')} 25%, ${orBlank(
+		solveTypes.efmSolve,
+		'hsl(300, 100%, 75%)'
+	)} 25.01% 50%, ${orBlank(mission.tpSolve, '#9146ff')} 50.01% 75%, ${orBlank(
+		solveTypes.soloSolve,
+		'#00ffff'
+	)} 75.01%)`;
 
 	const solvers = [
 		solveTypes.normalSolve ? 'by a team' : null,
 		solveTypes.efmSolve ? 'via EFM' : null,
 		mission.tpSolve ? 'on Twitch Plays' : null,
 		solveTypes.soloSolve ? 'solo' : null
-	].flatMap((solver) => solver ?? []);
+	].flatMap(solver => solver ?? []);
 	const title =
-		solvers.length === 0
-			? "This mission hasn't been solved."
-			: `This mission has been solved ${listify(solvers)}.`;
+		solvers.length === 0 ? "This mission hasn't been solved." : `This mission has been solved ${listify(solvers)}.`;
 </script>
 
 <div class="mission-name">{mission.name}</div>
@@ -41,10 +39,8 @@
 	{#if bombs.length > 1}
 		{bombs.length} B ·
 	{/if}
-	{statBomb.modules}
-	{bombs.length > 1 ? 'Mods' : 'Modules'} · {formatTime(statBomb.time)} · {statBomb.strikes}
-	Strikes · {statBomb.widgets}
-	Widgets
+	{pluralize(statBomb.modules, bombs.length > 1 ? 'Mod' : 'Module')} · {formatTime(statBomb.time)} ·
+	{pluralize(statBomb.strikes, 'Strike')} · {pluralize(statBomb.widgets, 'Widget')}
 	{#if mission.factory !== null}
 		· {mission.factory}
 	{/if}
