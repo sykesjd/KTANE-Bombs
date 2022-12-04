078a19f3d74bc33e7725af8d6a8237e0ec9ba86e
Flamanis <Flamanis@users.noreply.github.com>
2022-12-04 07:27:29 -0600

    Use URL parsing for Mission ID upload (#67)
    * Change the parse in the MissionPack Upload
    
    Change the MissionPack ID field to parse the input as a number or a URL that points to a steamcommunity.com hostname.
    
    * Use trimmed string
    
    * Account for extra characters in the retrieved id and run prettier
    
    * Revert "Account for extra characters in the retrieved id and run prettier"
    
    This reverts commit 8b0419120b6999cb005aba0dbf0a169a5fe8e429.
    
    * Add final fallback. Run proper prettier extension
    
    * Steam ID field unchanged and gives info on error
    
    * Revert "Steam ID field unchanged and gives info on error"
    
    This reverts commit c0622f9d55b06a13a6ae6dc5c624d33a797a3c6e.
    
    * Change to use Validate instead of Parse
    
    Includes same changes I made to Input.svelte in #69
    
    * Still parse steam ID for submission
    
    Lul. That's a bad oversight.
    
    * Change return, same as in #69
    
    * Add Input forceValidate property. As in #69
    
    Co-authored-by: LuminoscityTim <luminoscity@gmail.com>

--- src/routes/upload/_MissionPackSection.svelte
+++ src/routes/upload/_MissionPackSection.svelte
@@ -9,17 +9,44 @@
 		steamId: ''
 	};
 
-	$: valid = pack.name.length > 0 && pack.steamId.length > 0;
+	let invalid = false;
+	let valid = false;
+	let inputtedId = '';
+	$: {
+		pack.steamId = getSteamID(inputtedId);
+		valid = !invalid && pack.name.length > 0 && pack.steamId.length > 0;
+	}
 
-	function parseSteamId(str: string): string | null {
+	function getSteamID(str: string): string {
 		let trimmed = str.trim();
 		if (isOnlyDigits(trimmed)) return trimmed;
-		else {
-			trimmed = trimmed.replace('https://steamcommunity.com/sharedfiles/filedetails/?id=', '');
-			trimmed = trimmed.substring(0, trimmed.search(/[^0-9]/));
-			if (isOnlyDigits(trimmed)) return trimmed;
+
+		let url: URL | null = null;
+		try {
+			url = new URL(trimmed);
+		} catch (e: any) {
+			return '';
 		}
-		return null;
+
+		if (url?.hostname !== 'steamcommunity.com') return '';
+
+		let id = url?.searchParams?.get('id');
+		if (id === null) return '';
+
+		if (isOnlyDigits(id)) return id;
+
+		id = id.substring(0, id.search(/[^0-9]/));
+		if (isOnlyDigits(id)) return id;
+
+		return '';
+	}
+
+	function validateSteamID(str: string): string | boolean {
+		let id = getSteamID(str);
+		if (id === '') {
+			return 'Invalid Steam Workshop URL or Workshop ID.';
+		}
+		return '';
 	}
 
 	function upload() {
@@ -44,12 +71,12 @@
 <div class="block flex grow">
 	<Input label="Name" id="pack-name" required bind:value={pack.name} />
 	<Input
-		label="Steam ID"
+		label="Steam ID / Workshop URL"
 		id="pack-steam-id"
-		parse={parseSteamId}
-		validate={value => value != null}
-		instantFormat={false}
-		bind:value={pack.steamId} />
+		validate={validateSteamID}
+		required
+		bind:invalid
+		bind:value={inputtedId} />
 </div>
 <div class="block">
 	<button on:click={upload} disabled={!valid}>Upload</button>
