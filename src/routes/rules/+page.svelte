2d78aaa2febab83cd462badb5f5b172c80412bab
BlvdBroken <skazimirowicz@gmail.com>
2023-01-26 12:35:44 -0800

    Add static svelte rules page

---
+++ src/routes/rules/+page.svelte
@@ -0,0 +1,43 @@
+<svelte:head>
+	<title>Rules</title>
+</svelte:head>
+
+<h1 class="header">Rules</h1>
+
+<body>
+	<p>
+		In order for a bomb to show on your profile, you must submit it here with the names of everyone who helped solve the bomb and their roles with proper verification. 
+		Proper verification includes a logfile or recording of the bomb. 
+		In addition, you must follow the rules below in order for your solve not to be disqualified.
+	</p>
+	
+	<p>Actions/mods that will invalidate your solve include:</p>
+	<ul>
+		<li>Using any feature not allowed by turning Advantageous Features off.</li>
+ 		<li>Playing in any timer mode besides normal mode.</li>
+ 		<li>Taking screenshots or recordings of the bomb during a team solve to aid in solving.</li>
+		<li>Using local, private, or unlisted mods to replace modules on the bomb with easier or different versions.</li>
+		<li>Using local, private, or unlisted mods to give an unfair advantage such as modifying the logging or fixing bomb RNG.</li>
+		<li>Using the Twitch Plays or Unity Explorer mods during a solve.</li>
+		<li>Using advantageous features from the Command Line mod.</li>
+		<li>Using the Magic Stopwatch, Manual Viewer, or Tally Counter mods during your solve.</li>
+		<li>Playing in the Manual room.</li>
+		<li>Setting the mission seed to anything besides -1.</li>
+		<li>Using a mod to change the fonts on the bomb.</li>
+		<li>Viewing the logfile of the bomb before it is solved.</li>
+		<li>Having Logging appear on a bomb with an ALL_MODS pool.</li>
+		<li>Revealing module names using Profile Revealer.</li>
+		<li>Using mod settings to make a module significantly easier than default without reason.</li>
+		<li>Using a manual not accessible on the repo.</li>
+		<li>Using any needy listed below when given the choice of needy.</li>
+        <ul>
+			<li>Literally Nothing</li>
+			<li>Hearthur</li>
+			<li>Kawaiitlyn</li>
+		</ul>
+		<li>A defuser taking or using any external notes or props to aid in solving. This excludes using props to aid with colorblindness.</li>
+		<li>An expert viewing the bomb during the solve. </li>
+
+	</ul>
+</body>
+
