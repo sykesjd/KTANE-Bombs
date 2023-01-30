d7aab036147de5fe20498960706ddc35f01e5fe2
BlvdBroken <skazimirowicz@gmail.com>
2023-01-29 22:27:24 -0800

    Edit rules and add mission upload rules

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -1,3 +1,5 @@
+<script lang="ts">import { reservedSearchStrings } from '$lib/util';</script> 
+
 <svelte:head>
 	<title>Rules</title>
 </svelte:head>
@@ -5,19 +7,23 @@
 <h1 class="header">Rules</h1>
 
 <body>
+	<h2>Rules for Uploading Solves</h2>
+
 	<p>
-		In order for a bomb to show on your profile, you must submit it here with the names of everyone who helped solve the bomb and their roles with proper verification. 
-		Proper verification includes a logfile or recording of the bomb. 
-		In addition, you must follow the rules below in order for your solve not to be disqualified.
+		In order for a bomb to show on your profile, you must submit it here with the names of everyone who helped solve the
+		bomb and their roles with proper verification. Proper verification includes a logfile or recording of the bomb.
 	</p>
-	
+
 	<p>Actions/mods that will invalidate your solve include:</p>
 	<ul>
-		<li>Using any feature not allowed by turning Advantageous Features off.</li>
- 		<li>Playing in any timer mode besides normal mode.</li>
- 		<li>Taking screenshots or recordings of the bomb during a team solve to aid in solving.</li>
+		<li>Using any feature enabled by turning off "Disable Advantageous Features".</li>
+		<li>Playing in any timer mode besides Normal mode.</li>
+		<li>Taking screenshots or recordings of the bomb during a team solve to aid in solving.</li>
 		<li>Using local, private, or unlisted mods to replace modules on the bomb with easier or different versions.</li>
-		<li>Using local, private, or unlisted mods to give an unfair advantage such as modifying the logging or fixing bomb RNG.</li>
+		<li>
+			Using local, private, or unlisted mods to give an unfair advantage such as modifying the logging or fixing bomb
+			RNG.
+		</li>
 		<li>Using the Twitch Plays or Unity Explorer mods during a solve.</li>
 		<li>Using advantageous features from the Command Line mod.</li>
 		<li>Using the Magic Stopwatch, Manual Viewer, or Tally Counter mods during your solve.</li>
@@ -30,14 +36,38 @@
 		<li>Using mod settings to make a module significantly easier than default without reason.</li>
 		<li>Using a manual not accessible on the repo.</li>
 		<li>Using any needy listed below when given the choice of needy.</li>
-        <ul>
+		<ul>
 			<li>Literally Nothing</li>
 			<li>Hearthur</li>
 			<li>Kawaiitlyn</li>
 		</ul>
-		<li>A defuser taking or using any external notes or props to aid in solving. This excludes using props to aid with colorblindness.</li>
-		<li>An expert viewing the bomb during the solve. </li>
+		<li>
+			A defuser taking or using any external notes or props to aid in solving. This excludes using props to aid with
+			colorblindness.
+		</li>
+		<li>An expert viewing the bomb during the solve.</li>
+	</ul>
 
+	<h2>Rules for Uploading Missions</h2>
+
+	<p>Missions that break any of the below rules will lead to them being denied by the maintainers:</p>
+	<ul>
+		<li>It contains less than 47 modules.</li>
+		<ul><li>Modules that do nothing such as Literally Nothing do not count towards this total.</li></ul>
+		<li>It contains the module Logging.</li>
+		<li>It contains too many repeat modules.</li>
+		<li>The mission's contents are too similar to those of another mission.</li>
+		<li>The mission name is the same as another's on this website.</li>
+		<li>The mission name is vulgar or offensive.</li>
+		<li>
+			The mission name contains any of these reserved character sequences:
+			{#each reservedSearchStrings as str, index}
+				<b>{str}</b>{index < reservedSearchStrings.length - 1 ? ', ' : '.'}
+			{/each}
+		</li>
+		<li>
+			The mission is too easy according to the maintainers. If your mission is denied for this reason, we will give
+			pointers to fix this.
+		</li>
 	</ul>
 </body>
-
